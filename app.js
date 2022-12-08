9af36ad473eed48bf2208ebe08fb425ef3788a1c
Omran Tlajeh <81022408+omrantal@users.noreply.github.com>
2022-12-08 11:49:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -42,13 +42,13 @@ mongoose.connect(process.env.CONNECTION_STRING, {
     console.log(err)
   })
 
-/*// Development
+// Development
 app.listen(3000, () => {
   console.log('server is running on port http://localhost:3000')
-})*/
+})
 
-// Production
+/*// Production
 var server = app.listen(process.env.PORT || 3000, function () {
   var port = server.address().port
   console.log("Express is working on port " + port)
-})
+})*/
